f74b0115a6454fe4cfbcc3962080429e483bca87
Julie <somewhatvaguely@gmail.com>
2017-11-25 14:14:41 +0000

    changes to price controller

--- assets/js/priceController.js
+++ assets/js/priceController.js
@@ -5,19 +5,13 @@ window.app.controller('PriceController', ($scope, $http) => {
    		$scope.items.push($scope.newItem);
 	}
 
-	var items = {
-		'Tesco':
-			[{ name: 'Milk', 	price: '3.42' },
-			 { name: 'Bread', 	price: '6.73' },
-			 { name: 'Eggs', 	price: '0.06' },
-			 { name: 'Cereal', 	price: '8.36' },
-			 { name: 'Beer', 	price: '7.70' },
-			 { name: 'Banana', 	price: '4.26' },
-			 { name: 'Apple',	price: '4.32' },
-			 { name: 'Pizza',	price: '7.77' },
-			 { name: 'Pepsi', 	price: '0.63' }]
+	var items = {};
 
-	}
+	$http.get('stores.json').then((response) => {
+		items = response; 
+		console.log(items)
+	})
+
 
 	for (var item in items.Tesco) {
 		console.log(items.Tesco[item].price)
