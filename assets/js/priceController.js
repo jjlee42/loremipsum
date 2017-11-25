ddf2122979515bc1cc5e76a3d8e02b6be5c78777
barbaraojei <barbaraojei@gmail.com>
2017-11-25 13:48:40 +0000

    Implemented adding items to the list

--- assets/js/priceController.js
+++ assets/js/priceController.js
@@ -1,4 +1,7 @@
 window.app.controller('PriceController', ($scope, $http) => {
+	$scope.items = ['cabbage', 'umbrella', 'steam train'];
 
-
+	$scope.add = function (){
+   		$scope.items.push($scope.newItem);
+	}
 })
