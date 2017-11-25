a291f7f9085f80ee145cc69db3b0abbbb1aec5ed
Julie <somewhatvaguely@gmail.com>
2017-11-25 13:52:34 +0000

    fixed

--- assets/js/priceController.js
+++ assets/js/priceController.js
@@ -4,4 +4,20 @@ window.app.controller('PriceController', ($scope, $http) => {
 	$scope.add = function (){
    		$scope.items.push($scope.newItem);
 	}
+	var req = {
+	 method: 'OPTIONS',
+	 url: 'http://api.walmartlabs.com/v1/search?query=milk&format=json&apiKey=4xw4ftrc4fce5aep3duamcfu',
+	 headers: {
+	   'Access-Control-Allow-Origin': '*',
+	   'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+	   'Access-Control-Allow-Headers': 'X-Requested-With,content-type',
+	   'Access-Control-Allow-Credentials': true
+	 },
+	 data: { test: 'test' }
+	}
+
+	$http(req).then((response) => {
+		console.log(response)
+	})
+
 })
